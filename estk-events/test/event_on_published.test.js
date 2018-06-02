a0402c9aab709823a8d14332992aca22648634dc
Dave Foley <davidmfoley@gmail.com>
2018-06-02 12:46:24 -0700

    Add test for onPublished

---
+++ estk-events/test/event_on_published.test.js
@@ -0,0 +1,33 @@
+// @flow weak
+import { describe, beforeEach, it } from 'mocha';
+import EventStore from '../src/event_store';
+
+describe('publishing an event', () => {
+  let store;
+
+  beforeEach(async () => {
+    store = await EventStore({
+      storage: {
+        publish: e => Promise.resolve(e),
+        close: () => Promise.resolve(),
+        getEventStream: () => Promise.resolve(({}: any))
+      }
+    })
+  });
+
+  it('invokes onPublished handlers', () => {
+    const publishRequest = {
+      targetType: 'book',
+      action: 'create',
+      targetId: '42',
+      data: {name: 'foo'}
+    };
+
+    return new Promise(resolve => {
+      store.onPublished(() => resolve());
+
+      store.publish(publishRequest);
+    });
+  });
+});
+
