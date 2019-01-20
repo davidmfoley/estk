7c58138fa4831b94eee43c9cacb62ff96b1e4d5c
Dave Foley <davidmfoley@gmail.com>
2019-01-19 16:31:29 -0800

    The great tsconfig battle of 2019

--- estk-read-models-pg/src/types.ts
+++ estk-read-models-pg/src/types.ts
@@ -6,13 +6,13 @@ export type PostgresReadModel = {};
 export type ReadModelLookup = string | string[] | Object;
 
 export type ReadModelActions = {
-  get: (lookup: ReadModelLookup) => Promise<Object>;
-  getAll: (lookup: ReadModelLookup) => Promise<Object[]>;
+  get: (lookup: ReadModelLookup) => Promise<any>;
+  getAll: (lookup: ReadModelLookup) => Promise<any[]>;
   create: (id: string, data: Object) => Promise<void>;
-  merge: (lookup: ReadModelLookup, fieldsToMerge: Object) => Promise<void>;
-  update: (id: string, data: Object) => Promise<void>;
-  createOrMerge: (id: string, data: Object) => Promise<void>;
-  createOrReplace: (id: string, data: Object) => Promise<void>;
+  merge: (lookup: ReadModelLookup, fieldsToMerge: any) => Promise<void>;
+  update: (id: string, data: any) => Promise<void>;
+  createOrMerge: (id: string, data: any) => Promise<void>;
+  createOrReplace: (id: string, data: any) => Promise<void>;
   delete: (lookup: ReadModelLookup) => Promise<void>;
 };
 
@@ -24,11 +24,27 @@ export type ReadModelsConfig = {
   };
 };
 
+type EventHandler = (
+  event: Event,
+  actions: ReadModelActions
+) => Promise<void>;
+
+type EventHandlerMap = {
+  [x: string]: EventHandler
+};
+
+type EventHandlerMapMap = {
+  [x: string]: EventHandler | EventHandlerMap
+}
+
+type ReadModelEventConfig = EventHandler | EventHandlerMapMap;
+
 export type ReadModelConfig = {
   name: string,
   version: number,
   fields: any,
-  defaultValue?: any
+  defaultValue?: any,
+  events: ReadModelEventConfig
 };
 
 export type ReadModel = any;
