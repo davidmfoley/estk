50bb367900e415f355e3d1a2e68c3336f8fdf82f
Dave Foley <davidmfoley@gmail.com>
2020-01-04 09:56:43 -0800

    Prettier

--- estk-pg/src/wrap_query.ts
+++ estk-pg/src/wrap_query.ts
@@ -1,15 +1,19 @@
 import { QueryResult } from 'pg';
 import { DatabaseQuery, ResultSet } from './types';
 
-export default (query: Function, debug: Function) => ({ sql, params = [] }: DatabaseQuery): Promise<ResultSet> => {
+export default (query: Function, debug: Function) => ({
+  sql,
+  params = [],
+}: DatabaseQuery): Promise<ResultSet> => {
   params = params || [];
-  let start = Date.now()
+  let start = Date.now();
   return query(sql, params).then(
     (result: QueryResult) => {
       const elapsed = Date.now() - start;
       debug(sql, elapsed + 'ms', 'success', result.rowCount, 'rows');
       return result.rows;
-    }, (err: Error) => {
+    },
+    (err: Error) => {
       var elapsed = Date.now() - start;
       debug(sql, elapsed + 'ms', 'error', err.message);
       throw new Error(`SQL Error in transaction: ${err.message}\n${sql}`);
--- estk-pg/test/client.test.ts
+++ estk-pg/test/client.test.ts
@@ -9,7 +9,7 @@ describe('postgres client', () => {
   async function getClient() {
     const config = {
       url: process.env.DATABASE_URL_TEST || '',
-      poolSize: 10
+      poolSize: 10,
     };
     return await PostgresClient(config);
   }
@@ -20,19 +20,19 @@ describe('postgres client', () => {
 
   it('can do queries and stuff', async () => {
     await client.query({
-      sql: 'drop table if exists client_test'
+      sql: 'drop table if exists client_test',
     });
     await client.query({
-      sql: 'create table client_test(id varchar)'
+      sql: 'create table client_test(id varchar)',
     });
     await client.query({
-      sql: `insert into client_test(id) values('a')`
+      sql: `insert into client_test(id) values('a')`,
     });
     await client.query({
-      sql: `insert into client_test(id) values('b')`
+      sql: `insert into client_test(id) values('b')`,
     });
     const rows = await client.query({
-      sql: `select * from client_test order by id`
+      sql: `select * from client_test order by id`,
     });
     expect(rows.length).to.eq(2);
     expect(rows[0].id).to.eq('a');
@@ -42,39 +42,40 @@ describe('postgres client', () => {
   describe('transactions', async () => {
     beforeEach(async () => {
       await client.query({
-        sql: 'drop table if exists client_test'
+        sql: 'drop table if exists client_test',
       });
       await client.query({
-        sql: 'create table client_test(id varchar)'
+        sql: 'create table client_test(id varchar)',
       });
     });
+
     it('handles commit', async () => {
       let transaction = await client.transaction();
       await transaction.query({
-        sql: `insert into client_test(id) values('a')`
+        sql: `insert into client_test(id) values('a')`,
       });
       let insideTransaction = await transaction.query({
-        sql: `select count(*) as row_count from client_test`
+        sql: `select count(*) as row_count from client_test`,
       });
       expect(insideTransaction[0].row_count).to.eq(1);
       let duringTransaction = await client.query({
-        sql: `select count(*) as row_count from client_test`
+        sql: `select count(*) as row_count from client_test`,
       });
       expect(duringTransaction[0].row_count).to.eq(0);
       await transaction.commit();
       let afterTransaction = await client.query({
-        sql: `select count(*) as row_count from client_test`
+        sql: `select count(*) as row_count from client_test`,
       });
       expect(afterTransaction[0].row_count).to.eq(1);
     });
     it('handles rollback', async () => {
       let transaction = await client.transaction();
       await transaction.query({
-        sql: `insert into client_test(id) values('a')`
+        sql: `insert into client_test(id) values('a')`,
       });
       await transaction.rollback();
       let afterRollback = await client.query({
-        sql: `select count(*) as row_count from client_test`
+        sql: `select count(*) as row_count from client_test`,
       });
       expect(afterRollback[0].row_count).to.eq(0);
     });
