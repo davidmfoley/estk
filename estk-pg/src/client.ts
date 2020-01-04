787acd0b0d7b7367abf0b6e2f4d1636d5921f6fe
Dave Foley <davidmfoley@gmail.com>
2020-01-04 09:55:07 -0800

    Clean up client logic

--- estk-pg/src/client.ts
+++ estk-pg/src/client.ts
@@ -9,31 +9,34 @@ const debug = Debug('estk-pg.client');
 
 type PostgresConfig = {
   url: string;
-  poolSize?: number
+  poolSize?: number;
 };
+
 type Connection = {
-  client: any;
+  client: DatabaseClient;
   done: Function;
 };
 
-let txPool: Pool;
-let nontxPool: Pool;
-
-export default function init(dbConfig: PostgresConfig): Promise<DatabaseClient> {
+export default function init(
+  dbConfig: PostgresConfig
+): Promise<DatabaseClient> {
   pg.defaults.poolSize = dbConfig.poolSize || 10;
 
   if (dbConfig.url) {
     return db(dbConfig);
   } else {
-    debug('configuration error! Could not connect to database! no URL specfied!');
-    return Promise.reject(new Error('Configuration error - could not connect to db'));
+    debug(
+      'configuration error! Could not connect to database! no URL specfied!'
+    );
+    return Promise.reject(
+      new Error('Configuration error - could not connect to db')
+    );
   }
 }
 
-function wrap(client: any): DatabaseClient {
+function wrap(client: pg.PoolClient, txPool: pg.Pool): DatabaseClient {
   return {
     query: wrapQuery(client.query.bind(client), debug),
-    transaction: () => PostgresTransaction(txPool)
     transaction: () => PostgresTransaction(txPool),
     close: async () => {
       client.release();
@@ -46,7 +49,7 @@ function parseUrl(databaseUrl: string) {
   var dbUrl = url.parse(databaseUrl);
 
   if (dbUrl.auth) {
-    var auth = dbUrl.auth.split(":");
+    var auth = dbUrl.auth.split(':');
     settings.user = auth[0];
     settings.password = auth[1];
   }
@@ -63,58 +66,27 @@ function createPool(name: string, url: string) {
 }
 
 function db(dbConfig: PostgresConfig): Promise<DatabaseClient> {
-  txPool = txPool || createPool('tx', dbConfig.url);
-  nontxPool = nontxPool || createPool('nontx', dbConfig.url);
+  const txPool = createPool('tx', dbConfig.url);
+  const nontxPool = createPool('nontx', dbConfig.url);
   pg.types.setTypeParser(1184, stringValue => stringValue);
   pg.types.setTypeParser(1114, stringValue => stringValue);
   pg.types.setTypeParser(1700, parseFloat); // convert bigints to JS int (needed because COUNT returns a bigint)
 
-  pg.types.setTypeParser(20, function (val) {
+  pg.types.setTypeParser(20, function(val) {
     return parseInt(val, 10);
   });
 
-  function connect(): Promise<Connection> {
-    return new Promise((resolve, reject) => {
-      nontxPool.connect(function (err, client, done) {
-        if (err) return reject(err);
-        resolve({
-          client: wrap(client),
-          done
-        });
-      });
-    });
-  }
-
+  const query = wrapQuery(nontxPool.query.bind(nontxPool), debug);
   const database = {
-    connect,
     transaction: () => PostgresTransaction(txPool),
-    query
+    query,
+    close: async () => {
+      await txPool.end();
+      await nontxPool.end();
+    },
   };
 
   return query({
-    sql: 'select 1'
+    sql: 'select 1',
   }).then(() => database);
-
-  function query({
-    sql,
-    params
-  }: DatabaseQuery): Promise<ResultSet> {
-    return connect().then(({
-      client,
-      done
-    }) => {
-      debug(sql, params);
-      return client.query({
-        sql,
-        params
-      }).then((result: ResultSet) => {
-        done();
-        return result;
-      }, (err: Error) => {
-        debug("SQL Error: " + err.message, sql);
-        done();
-        throw err;
-      });
-    });
-  }
 }
