043010f08a5d0632f5474fb2356ca15967ad9379
Dave Foley <davidmfoley@gmail.com>
2020-01-04 09:56:08 -0800

    Simplify transaction

--- estk-pg/src/transaction.ts
+++ estk-pg/src/transaction.ts
@@ -1,54 +1,30 @@
 import Debug from 'debug';
 import { DatabaseTransaction, DatabaseQuery, ResultSet } from './types';
-import { Pool, PoolClient, QueryResult, QueryArrayResult } from 'pg';
+import { Pool, PoolClient } from 'pg';
 import wrapQuery from './wrap_query';
 
 const debug = Debug('estk-pg.transaction');
 
-export default function PostgresTransaction(txPool: Pool): Promise<DatabaseTransaction> {
-  const buildQueryResult = (pgResult: QueryResult): ResultSet => {
-    return pgResult.rows;
+export default async function PostgresTransaction(
+  txPool: Pool
+): Promise<DatabaseTransaction> {
+  const client = await txPool.connect();
+  const wrappedQuery = wrapQuery(client.query.bind(client), debug);
+
+  await wrappedQuery({ sql: 'BEGIN;' });
+
+  const commit = async () => {
+    await wrappedQuery({ sql: 'COMMIT;' });
+    await client.release();
+  };
+  const rollback = async () => {
+    await wrappedQuery({ sql: 'ROLLBACK;' });
+    await client.release();
   };
 
-  return new Promise((resolve, reject) => {
-    txPool.connect((
-      err: Error | undefined,
-      client: PoolClient,
-      done: (err: any) => void
-    ) => {
-      if (err) {
-        return reject(err);
-      }
-
-      const wrappedQuery = wrapQuery(client.query.bind(client), debug);
-
-      function commit(): Promise<any> {
-        return wrappedQuery({ sql: 'COMMIT;' });
-      }
-
-      function rollback(): Promise<any> {
-        return wrappedQuery({ sql: 'ROLLBACK;' });
-      }
-
-      function query({
-        sql,
-        params = []
-      }: DatabaseQuery): Promise<ResultSet> {
-        return wrappedQuery({sql, params}).catch(
-          (err: Error) => {
-          return rollback().then(() => {
-            throw new Error(`SQL Error in transaction: ${err.message}\n${sql}`);
-          });
-        });
-      }
-
-      wrappedQuery({ sql: 'BEGIN;'}).then(() => (
-        resolve({
-          query,
-          commit,
-          rollback
-        })
-      ), reject);
-    });
-  });
+  return {
+    commit,
+    rollback,
+    query: wrappedQuery,
+  };
 }
