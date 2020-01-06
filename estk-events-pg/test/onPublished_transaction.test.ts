90773e7a0a527a712f6ef78ada26b965e82e1bf6
Dave Foley <davidmfoley@gmail.com>
2020-01-06 07:09:39 -0800

    test sketch

--- estk-events-pg/test/onPublished_transaction.test.ts
+++ estk-events-pg/test/onPublished_transaction.test.ts
@@ -1,3 +1,10 @@
 import { describe, it } from 'mocha';
+import { createEventStore } from 'estk-events';
+import { PostgresClient } from 'estk-pg';
+import PostgresEventStorage from '../src/event_storage';
 
-describe('onPublished', () => {});
+describe('onPublished', () => {
+  it('rolls back on error', async () => {});
+
+  it('commits if no errors', async () => {});
+});
