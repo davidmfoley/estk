9557bdaf804a644560e1a78beb396dda4ce02921
Dave Foley <davidmfoley@gmail.com>
2020-10-24 13:43:19 -0700

    Fix up amqp-connector paths

--- estk-amqp-connector/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  AmqpMessageBus: require('./lib/amqp_message_bus'),
-};
---
+++ estk-amqp-connector/src/index.ts
@@ -0,0 +1,4 @@
+import AmqpMessageBus from './amqp_message_bus';
+export default {
+  AmqpMessageBus,
+};
